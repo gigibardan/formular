14dc9a8cce558b35a4e34b7739daac88298865ce
gigibardan <gheorghe.bardan@gmail.com>
2025-02-15 18:15:08 +0200

    Fix static export configuration3

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import FormularInscriere from '@/components/FormularInscriere'
 export default function Home() {
   return (
     <main className="min-h-screen bg-white p-4">
+      <h1>TechMinds Academy</h1>
       <FormularInscriere />
     </main>
   )
